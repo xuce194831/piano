9a243b1e1e677fdd23b2dbcd1ee9d3c8eaba7193
vincent040 <18620373040@163.com>
2017-09-27 01:00:08 -0700

    修改了默认音量

--- src/ts.c
+++ src/ts.c
@@ -30,8 +30,8 @@ void ts_trace(int ts, struct coordinate *coor, bool *released)
 		read(ts, &buf, sizeof(buf));
 
 		// have released the touch-panel
-		if(buf.type == EV_ABS &&
-		   buf.code == ABS_PRESSURE &&
+		if(buf.type == EV_KEY &&
+		   buf.code == BTN_TOUCH &&
 		   buf.value == 0)
 		{
 			*released = true;
